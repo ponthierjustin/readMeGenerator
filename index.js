589779b29cee943d95f8e60ed6c9f88979ce8da8
Justin <ponthierjustin@gmail.com>
2020-06-25 19:20:26 -0400

    got basic question and answer working;

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 // require fs
 // require inquirier
+const fs = require('fs');
+const inquirer = require('inquirer');
 
+
+
+/* 
+inquirer
+  .prompt([
+    {
+      name: 'faveReptile',
+      message: 'What is your favorite reptile?',
+    },
+  ])
+  .then(answers => {
+    console.info('Answer:', answers.faveReptile);
+  }); */
 // array of questions for user
 //https://www.npmjs.com/package/inquirer#questions
-const questions = [
-
-];
+const questions = inquirer.prompt([
+    {
+        name: 'userName',
+        message: 'what is your username',
+    },
+])
+.then(answers => {
+    console.info('username:', answers.userName);
+});
 
 // function to write README file
 function writeToFile(fileName, data) {
