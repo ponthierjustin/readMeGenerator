da453cda6e938ef16dcea1044ef2270161ea0137
Justin <ponthierjustin@gmail.com>
2020-06-25 14:50:57 -0400

    built start up

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+// require fs
+// require inquirier
+
 // array of questions for user
+//https://www.npmjs.com/package/inquirer#questions
 const questions = [
 
 ];
@@ -6,9 +10,12 @@ const questions = [
 // function to write README file
 function writeToFile(fileName, data) {
 }
+// use the fs package
 
 // function to initialize program
 function init() {
+
+    // use the inquirier package
 
 }
 
