4d653025871c72c1a21a1462c35294d0d1e91240
Justin <ponthierjustin@gmail.com>
2020-06-25 22:00:34 -0400

    added questions and got answer outputs

--- index.js
+++ index.js
@@ -5,27 +5,37 @@ const inquirer = require('inquirer');
 
 
 
-/* 
-inquirer
-  .prompt([
-    {
-      name: 'faveReptile',
-      message: 'What is your favorite reptile?',
-    },
-  ])
-  .then(answers => {
-    console.info('Answer:', answers.faveReptile);
-  }); */
 // array of questions for user
 //https://www.npmjs.com/package/inquirer#questions
 const questions = inquirer.prompt([
     {
+        type: 'input',
         name: 'userName',
-        message: 'what is your username',
+        message: 'what is your username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is your email?',
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'what is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'shortDesc',
+        message: 'add a short desciprion of your project',
+    },
+    {
+        type: 'confirm',
+        name: 'mLicense',
+        message: 'would you like to add a MIT license?',
     },
 ])
-.then(answers => {
-    console.info('username:', answers.userName);
+.then(data => {
+    console.info(data);
 });
 
 // function to write README file
